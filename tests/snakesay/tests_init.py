272c4c1f61a7b641a5685dc76def0cc6fdfcbb34
Manuel A. Diaz <manuel.ade@gmail.com>
2026-01-04 17:43:07 +0100

    Include new tests

--- tests/snakesay/tests_init.py
+++ tests/snakesay/tests_init.py
@@ -1,5 +1,17 @@
-from snakesay.snake import generate_list
+from snakesay import snake
+
+BUBBLE_HI = " _____\n( hi! )\n ‾‾‾‾‾"
+
+SNAKE_HI = " _____\n( hi! )\n ‾‾‾‾‾ \n  \\\n   \\    __\n    \\  {oo}\n       (__)\\\n         λ \\\\\n           _\\\\__\n          (_____)_\n         (________)Oo°\n"
 
 
 def test_generate_list():
-    assert 42 == generate_list()
+    assert 42 == snake.generate_list()
+
+
+def test_snake_bubble():
+    assert BUBBLE_HI == snake.bubble("hi!")
+
+
+def test_snake_say():
+    assert SNAKE_HI == snake.say("hi!")
